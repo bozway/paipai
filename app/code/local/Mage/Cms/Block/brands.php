3b80a07c2764d35b00d7ed7185cfb141978342b6
Hao Cai <haocai@usc.edu>
2014-03-10 19:10:05 -0700

    implemented the brand list in the home page, added own block and template

---
+++ app/code/local/Mage/Cms/Block/brands.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Brands block content block
+ *
+ * @category   Mage
+ * @package    Mage_Cms
+ * @author     Hao Cai <hao.cai@bozway.com>
+ */
+class Mage_Cms_Block_Brands extends Mage_Core_Block_Template
+{
+    public function showAllBrands(){
+        $brand_array = array();
+        $subCats = Mage::getModel('catalog/category')->load(11)->getChildren();
+        $subCatIds = explode(',',$subCats);
+        if(count($subCatIds) > 1){
+            foreach($subCatIds as $subCatId){
+                $subCat = Mage::getModel('catalog/category')->load($subCatId);
+                if($subCat->getIsActive()){
+                    $brand = array(
+                        'img_src' => $subCat->getImageUrl(),
+                        'url' => $subCat->getUrl(),
+                        'name' => $subCat->getName()
+                    );
+                    $brand_array[] = $brand;
+                }
+            }
+        }
+        return $brand_array;
+    }
+}
+?>
